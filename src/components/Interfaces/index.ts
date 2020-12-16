0bae5cbeca9ecd26f0f4ee214f4d07f1408fbf37
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-16 15:34:45 -0600

    added new interfaces

--- src/components/Interfaces/index.ts
+++ src/components/Interfaces/index.ts
@@ -1,3 +1,28 @@
+interface MyFormProps {
+  id: Number;
+  ticker: string;
+  number_of_shares: Number;
+  short_or_long: string;
+  buy_price: Number;
+  sell_price: Number;
+  short_price: Number;
+  cover_price: Number;
+  day_or_swing: string;
+  start_date: string;
+  end_date: string;
+}
+
+interface AnalysisProps {
+  numberOfTrades?: Number;
+  winningTrades?: Number;
+  losingTrades?: Number;
+  dayTrades?: Number;
+  swingTrades?: Number;
+  typeOfTrade?: String;
+  startDate?: String;
+  endDate?: String;
+}
+
 interface Trade {
   id: Number;
   ticker: string;
@@ -12,4 +37,12 @@ interface Trade {
   end_date: string;
 }
 
+interface WindowSize {
+  screenWidth: Number;
+  screenHeight: Number;
+}
+
+export type { MyFormProps };
+export type { AnalysisProps };
 export type { Trade };
+export type { WindowSize };
