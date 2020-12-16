ebf3397de4d1019f5319f395669358e015e315c9
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-16 16:00:01 -0600

    added protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route } from "react-router";
 import "./App.css";
-// import { AttachAuthHeader } from "./services/http";
-// import { ProtectedRoute } from "./auth/protected-route";
+import { AttachAuthHeader } from "./services/http";
+import { ProtectedRoute } from "./auth/protected-route";
 import Main from "./components/Main";
 import Dashboard from "./components/dashboardComponents/Dashboard";
 import { EnterTrades } from "./components/formComponents/EnterTrades";
 
 function App() {
-  // const [isToken, setIsToken] = useState(false);
+  const [isToken, setIsToken] = useState(false);
   //this sets a default auth header to axios;
-  // AttachAuthHeader(setIsToken);
+  AttachAuthHeader(setIsToken);
 
   return (
     <div className="App">
       <Route exact path="/" component={Main} />
-      <Route exact path="/dashboard" component={Dashboard} />
-      <Route exact path="/enter-trades/:id?" component={EnterTrades} />
+      <ProtectedRoute
+        exact
+        path="/dashboard"
+        component={isToken ? Dashboard : Main}
+      />
+      <ProtectedRoute
+        exact
+        path="/enter-trades"
+        component={isToken ? EnterTrades : Main}
+      />
     </div>
   );
 }
