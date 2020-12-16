467d9ba90402ff3cab286ebddd3b5b659d259329
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-16 12:12:13 -0600

    removed token verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
 import { Route } from "react-router";
 import "./App.css";
-import { AttachAuthHeader } from "./services/http";
-import { ProtectedRoute } from "./auth/protected-route";
+// import { AttachAuthHeader } from "./services/http";
+// import { ProtectedRoute } from "./auth/protected-route";
 import Main from "./components/Main";
 import Dashboard from "./components/dashboardComponents/Dashboard";
 
 function App() {
   const [isToken, setIsToken] = useState(false);
   //this sets a default auth header to axios;
-  AttachAuthHeader(setIsToken);
+  // AttachAuthHeader(setIsToken);
 
   return (
     <div className="App">
       <Route exact path="/" component={Main} />
-      <ProtectedRoute
-        exact
-        path="/dashboard"
-        component={isToken ? Dashboard : Main}
-      />
+      <Route exact path="/dashboard" component={Dashboard} />
     </div>
   );
 }
