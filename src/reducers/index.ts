5c45b47daaebb79961696db55d6db7ec534b9ea9
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-16 16:56:06 -0600

    added form reducer for redux form

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer, FormStateMap } from "redux-form";
 import { tradeReducer } from "./Trades";
 import { Trade } from "../components/Interfaces";
 
 export interface StoreState {
+  form: FormStateMap;
   trades: Trade[];
 }
 
 export const reducers = combineReducers<StoreState>({
+  form: formReducer,
   trades: tradeReducer,
 });
