e62642a4ceec21dd510490b230c6394bc11fd2e0
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-16 11:53:51 -0600

    removed one slash in url

--- src/auth0-provider-with-history.tsx
+++ src/auth0-provider-with-history.tsx
@@ -15,7 +15,7 @@ export const Auth0ProviderWithHistory = ({ children }: IProps) => {
     <Auth0Provider
       domain={`${process.env.REACT_APP_AUTH0_DOMAIN}`}
       clientId={`${process.env.REACT_APP_AUTH0_CLIENT_ID}`}
-      redirectUri="https://main--trade-buddy.netlify.app//dashboard"
+      redirectUri="https://main--trade-buddy.netlify.app/dashboard"
       onRedirectCallback={onRedirectCallback}
       audience={`${process.env.REACT_APP_AUTH0_AUDIENCE}`}
     >
