220bbb805d2f28ba795ab4ccac04fd95b8ab875d
michael-williamson <michaeldavid1721@yahoo.com>
2021-02-23 12:13:08 -0600

    added createTrade action to reducer

--- src/reducers/Trades.ts
+++ src/reducers/Trades.ts
@@ -1,14 +1,21 @@
-import { DeleteTradeAction, GetTradesAction, UpdateTrade } from "../actions";
+import {
+  CreateTradeAction,
+  DeleteTradeAction,
+  GetTradesAction,
+  UpdateTrade,
+} from "../actions";
 import { Trade } from "../components/Interfaces/index";
 import { ActionTypes } from "../actions/types";
 
 //Todo[] = []  "= []" signifies default value of empty array
 export const tradeReducer = (
   state: Trade[] = [],
-  action: GetTradesAction | DeleteTradeAction | UpdateTrade
+  action: GetTradesAction | DeleteTradeAction | UpdateTrade | CreateTradeAction
 ) => {
   switch (action.type) {
     case ActionTypes.getUserTrades:
+      return [...action.payload];
+    case ActionTypes.createTrade:
       return [...action.payload];
     case ActionTypes.deleteTrade:
       return [...action.payload];
